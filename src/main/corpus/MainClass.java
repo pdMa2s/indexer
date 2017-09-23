84b1723fc2f185c5756ec4853b4c82924955a283
david <davidcruzferreira@ua.pt>
2017-09-23 20:00:00 +0100

    stopWords

--- src/main/corpus/MainClass.java
+++ src/main/corpus/MainClass.java
@@ -1,10 +1,15 @@
 import org.tartarus.snowball.ext.englishStemmer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainClass {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-    
+
         System.out.print("Please define the corpus Location: ");
         Scanner sc = new Scanner(System.in);
         String DirName = sc.nextLine();
@@ -12,12 +17,13 @@ public class MainClass {
         CorpusReader corpusReader = new DirIteratorCorpusReader(DirName);
         SimpleTokenizer st = new SimpleTokenizer();
         englishStemmer stemmer = new englishStemmer();
+        stopWordList[] =fillStopwordList();
 
-        while(corpusReader.hasDocument()){
+        while (corpusReader.hasDocument()) {
             String docContent = corpusReader.processDocument();
             System.out.println(docContent);
             String[] tokens = st.tokenize(docContent);
-            for (int i=0 ; i<tokens.length; i++){
+            for (int i = 0; i < tokens.length; i++) {
                 stemmer.setCurrent(tokens[i]);
                 stemmer.stem();
                 tokens[i] = stemmer.getCurrent();
@@ -25,4 +31,18 @@ public class MainClass {
             break;
         }
     }
+
+    public List<String> fillStopwordList() throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("StopWordList"));
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
+
+        while (line != null) {
+            sb.append(line);
+            line = br.readLine();
+        }
+        return sb.toString();
+    }
 }
+
