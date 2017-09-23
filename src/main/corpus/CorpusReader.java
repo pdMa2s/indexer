7a69da6ec7eeef4e0cea729eeb3324bd8bc24325
Pedro Matos <pedroguilhermematos@ua.pt>
2017-09-23 17:35:49 +0100

    iterate over a directory of files

--- src/main/corpus/CorpusReader.java
+++ src/main/corpus/CorpusReader.java
@@ -1,6 +1,4 @@
-import java.io.File;
-
-public abstract class CorpusReader {
-    public abstract String processDocument(File directory);
-
+public interface CorpusReader {
+    String processDocument();
+    boolean hasDocument();
 }
--- src/main/corpus/MainClass.java
+++ src/main/corpus/MainClass.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args){
     
-    System.out.print("Please define the corpus Location: ");
-    Scanner sc = new Scanner(System.in);
-    String DirName = sc.nextLine();
+        System.out.print("Please define the corpus Location: ");
+        Scanner sc = new Scanner(System.in);
+        String DirName = sc.nextLine();
 
-    DirIteratorCorpusReader dirReader = new DirIteratorCorpusReader(DirName);
+        CorpusReader corpusReader = new DirIteratorCorpusReader(DirName);
+
+        while(corpusReader.hasDocument()){
+            String docContent = corpusReader.processDocument();
+            System.out.println(docContent);
+            break;
+        }
+
     }
 }
